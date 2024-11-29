953e0d455e3f0c84c731500220579b1cd2c6d712
Lucas07155 <lucaspraxedes74@gmail.com>
2024-11-29 20:37:56 -0300

    commit

--- cadastrar_professor.php
+++ cadastrar_professor.php
@@ -9,6 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastrar professor</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="estilo_cads_prf.css">
 
 </head>
 <body>
