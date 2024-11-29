9ddb2f30cdea04bd2e0579c6c69ebbf2769a4c5e
Lucas07155 <lucaspraxedes74@gmail.com>
2024-11-29 18:51:13 -0300

    Initial commit

---
+++ logo.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar ALUNO</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="estilo_logo.css">
+</head>
+<body>
+    <nav id="menu-h">
+        <div class="logo">
+            <img src="logo.png" alt="Superfit Logo">
+        </div>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="planos_academia.php">Planos</a></li>
+            <li><a href="#">Sobre</a></li>
+            <li><a href="#">Contato</a></li>
+            <li><a href="login_aluno.php">Entrar</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
